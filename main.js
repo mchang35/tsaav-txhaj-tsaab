ff0352171eb0c9766e4acfb29a69316b50dd40f1
mchang35 <mandy_chang@brown.edu>
2021-10-14 01:01:09 -0400

    getting functions ready

--- main.js
+++ main.js
@@ -10,40 +10,55 @@ ready(() => {
 var eng_lang = true; // boolean for whether we are currently in English Language\
 
 var navbar_list = document.getElementsByClassName("navbar-expand-md");
-var navbar = navbar_list[0];
-console.log("navbar:");
-console.log(navbar);
+var navbar = navbar_list[0]; // gets the navigation bar
+// defines the where the navigation bar will be FIXED!
 var navbar_sticky_Y = window.scrollY + window.innerHeight;
-console.log("sticky Y:");
-console.log(navbar_sticky_Y);
-
-// define a function for when they click on each button in the navigation
 
 // when users scroll on the window, make sure that the navigation is sticky
 window.onscroll = function() {sticky_nav()};
-
-
 
 // define a function for hovering over the family tree
 
 // function that makes navigation sticky
 function sticky_nav() {
-    console.log("Classes of navbar:")
-    console.log(navbar.classList)
-    // let list = navbar.classList
     if (window.pageYOffset >= navbar_sticky_Y) {
-        // list.add("sticky");
-        // navbar.classList.toggle("sticky", true);
-        // if (!navbar.classList.contains("sticky")) {
             navbar.classList.add("sticky");
-        // }
     } else {
-        // list.remove("sticky");
-        // navbar.classList.toggle("sticky", false);
-        // if (navbar.classList.contains("sticky")) {
             navbar.classList.remove("sticky");
-        // }
     }
+}
+
+// define a function for when they click on each button in the navigation
+function top() {
+
+}
+
+function son() {
+
+}
+
+function brother() {
+
+}
+
+function husband() {
+
+}
+
+function father() {
+
+}
+
+function grandfather() {
+
+}
+
+function family() {
+
+}
+
+function memvideo() {
+
 }
 
 // function that switches whole website to Hmong
