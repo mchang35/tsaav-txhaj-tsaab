236638efe00d485ab25c3a30c10028df0d5ad680
mchang35 <mandy_chang@brown.edu>
2021-10-14 21:06:54 -0400

    Adding example images

--- main.js
+++ main.js
@@ -7,12 +7,28 @@ ready(() => {
 })
 
 // define a function for when they click on Hmong vs English
-var eng_lang = true; // boolean for whether we are currently in English Language\
+var eng_lang = true; // boolean for whether we are currently in English Language
 
 var navbar_list = document.getElementsByClassName("navbar-expand-md");
 var navbar = navbar_list[0]; // gets the navigation bar
-// defines the where the navigation bar will be FIXED!
+// defines where the navigation bar will be FIXED!
 var navbar_sticky_Y = window.scrollY + window.innerHeight;
+
+// defines where the program starts
+
+// defines where the Son portion starts
+
+// defines where the Brother portion starts
+
+// defines where the Husband portion starts
+
+// defines where the Father portion starts
+
+// defines where the Grandfather portion starts
+
+// defines where the Family Tree/Memories portion starts
+
+// defines where the Memorial Video portion starts
 
 // when users scroll on the window, make sure that the navigation is sticky
 window.onscroll = function() {sticky_nav()};
@@ -28,43 +44,52 @@ function sticky_nav() {
     }
 }
 
+// define a function that makes things appear as you scroll
+// make things VISIBLE vs NOT (they're always there though)
+
 // define a function for when they click on each button in the navigation
-// function top() {
-//
-// }
-//
-// function son() {
-//
-// }
-//
-// function brother() {
-//
-// }
-//
-// function husband() {
-//
-// }
-//
-// function father() {
-//
-// }
-//
-// function grandfather() {
-//
-// }
-//
-// function family() {
-//
-// }
-//
-// function memvideo() {
-//
-// }
+function top_of_page() {
+    document.body.scrollTop = 0; // for Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function program() {
+
+}
+
+function son() {
+
+}
+
+function brother() {
+
+}
+
+function husband() {
+
+}
+
+function father() {
+
+}
+
+function grandfather() {
+
+}
+
+function family() {
+
+}
+
+function memvideo() {
+
+}
 
 // function that switches whole website to Hmong
 function switch_to_hmong() {
     if (eng_lang == true) {
         document.getElementById("top").text = "Hmong for Top"; // translate "Top"
+        document.getElementById("program").text = "Hmong for Funeral Program"; // translate "Funeral Program"
         document.getElementById("son").text = "Hmong for As a Son"; // translate "As a Son"
         document.getElementById("brother").text = "Hmong for As a Brother"; // translate "As a Brother"
         document.getElementById("husband").text = "Hmong for As a Husband"; // translate "As a Husband"
@@ -80,6 +105,7 @@ function switch_to_hmong() {
 function switch_to_english() {
     if (eng_lang == false) {
         document.getElementById("top").text = "Top"; // translate "Top"
+        document.getElementById("program").text = "Funeral Program"; // translate "Funeral Program"
         document.getElementById("son").text = "As a Son"; // translate "As a Son"
         document.getElementById("brother").text = "As a Brother"; // translate "As a Brother"
         document.getElementById("husband").text = "As a Husband"; // translate "As a Husband"
