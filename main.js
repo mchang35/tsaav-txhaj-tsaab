1e10d1731b3465afb69811805070b4a4faaf49d9
mchang35 <mandy_chang@brown.edu>
2021-10-18 11:50:26 -0400

    got the sliding when scrolling in!!

--- main.js
+++ main.js
@@ -55,38 +55,80 @@ var times = [
 
 ////////////////////////////////////////////////////////////////////////////
 // SCROLLING TAKEN CARE OF HERE //
-const scrollOffset = 100;
+// const scrollOffset = 100;
+//
+// const scrollElement = document.querySelectorAll(".js-scroll");
+// // problem: only selects the FIRST element with this ^^
+//
+// const elementInView = (el, dividend = 1) => {
+//     const elementTop = el.getBoundingClientRect().top;
+//
+//     return (
+//         elementTop <=
+//         ((window.innerHeight || document.documentElement.clientHeight) / dividend)
+//     );
+// };
+//
+// const displayScrollElement = () => {
+//     scrollElement.classList.add('scrolled');
+// }
+//
+// const hideScrollElement = () => {
+//     scrollElement.classList.remove('scrolled');
+// }
+//
+// const handleScrollAnimation = () => {
+//     if (elementInView(scrollElement, scrollOffset)) {
+//         displayScrollElement();
+//     } else {
+//         hideScrollElement();
+//     }
+// }
+//
+// window.addEventListener('scroll', () => {
+//     handleScrollAnimation();
+// })
 
-const scrollElement = document.querySelector(".js-scroll");
+const scrollElements = document.querySelectorAll(".js-scroll");
 
-const elementInView = (el, offset = 0) => {
+const elementInView = (el, dividend = 1) => {
     const elementTop = el.getBoundingClientRect().top;
 
     return (
         elementTop <=
-        ((window.innerHeight || document.documentElement.clientHeight) - offset)
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
     );
 };
 
-const displayScrollElement = () => {
-    scrollElement.classList.add('scrolled');
-}
+const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
 
-const hideScrollElement = () => {
-    scrollElement.classList.remove('scrolled');
-}
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+};
 
 const handleScrollAnimation = () => {
-    if (elementInView(scrollElement, scrollOffset)) {
-        displayScrollElement();
-    } else {
-        hideScrollElement();
-    }
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        } else if (elementOutofView(el)) {
+            hideScrollElement(el)
+        }
+    })
 }
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     handleScrollAnimation();
-})
+});
 
 ////////////////////////////////////////////////////////////////////////////
 
